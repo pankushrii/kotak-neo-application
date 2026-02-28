8a9692703ab795935910d2c2d302c779a003dfe0
pankushrii <53987772+pankushrii@users.noreply.github.com>
2026-02-28 11:18:19 +0530

    Create kotakConfig.js

---
+++ api/kotakConfig.js
@@ -0,0 +1,17 @@
+// api/kotakConfig.js
+const apiConfigs = {
+  "1": {
+    accessToken: "a3de4fd6-da0b-49bc-8f11-256b84b5ec0f", // your Neo dashboard access token
+    neoFinKey: "neotradeapi",
+    mobileNumber: "+917000560918",
+    ucc: "YIVKF",
+    mpin: "1234" // <-- SET YOUR MPIN HERE (keep hardcoded)
+  }
+};
+
+// choose which config to use
+const ACTIVE_CONFIG_ID = "1";
+
+module.exports = {
+  apiConfig: apiConfigs[ACTIVE_CONFIG_ID]
+};
