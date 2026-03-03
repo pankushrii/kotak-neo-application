cccc85ac024d48e4bccb0c8173bd0117ca29b328
pankushrii <53987772+pankushrii@users.noreply.github.com>
2026-03-03 23:07:23 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -132,6 +132,7 @@ async function fetchMasterScripCsvAndCache(force, session, isOptionChain = false
 
   // Call the newly defined parser
   const rows = parseScripMasterCsv(csvText, isOptionChain);
+  console.log("rowsss",rows);
 
   SCRIP_CACHE = { 
     updatedAt: now, 
